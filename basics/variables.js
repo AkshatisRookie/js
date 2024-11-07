dc688d733b53f3f4688f85698811a5a7ccd2cfd2
Akshat Seth <u23ee101@eed.svnit.ac.in>
2024-11-07 05:30:46 +0000

    variablesnotesJS

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId="akshat"
+let accountemail="u23ee101@"
+let name;
+/*always use let to assume value nd all*/
+var pass="abcdef" /*do not use var*/
+console.log(accountId)
+console.table([accountId,accountemail,pass,name])
+console.log(typeof "akshat");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined only
