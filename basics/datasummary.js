f39973624195a6130581b8e53db271d81a1317eb
Akshat Seth <u23ee101@eed.svnit.ac.in>
2024-11-07 06:17:55 +0000

    basicKnowledge

---
+++ basics/datasummary.js
@@ -0,0 +1,15 @@
+// data is divide on the way how they are stored in memoery are PRIMITIVE AND NONPRIMITIVE
+/*PRIMITIVE-7
+:STRING,NUMBER,BOOLEAN,null,undefined,symbol,BigInt*/
+// refernce(nonprimitve)
+//Js is dynamically typed coz we dont need to define the tyoe while conversion\
+//array,objects,functions
+const bigNumber=2765237n //n means big number
+const hey=['age','akshat']
+let my={
+    name:'hitesh'
+    age:22
+}
+const myfucntion=function(){
+    console.log("hi")
+}
