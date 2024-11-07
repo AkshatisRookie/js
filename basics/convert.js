74a22245cd357ffddcf94d71c1a4efc3829a5727
Akshat Seth <u23ee101@eed.svnit.ac.in>
2024-11-07 05:31:16 +0000

    convertionTypesJS

---
+++ basics/convert.js
@@ -0,0 +1,17 @@
+let score="69"
+let valueinNo=Number(score)
+console.log(valueinNo)
+let score2="69avb"
+let valueinNo2=Number(score2)//gives 69 only
+let score3=null //object
+console.log(typeof score3);
+console.log(score2);
+let score4=undefined //NaN
+let loggedin=1
+let boolean=Boolean(loggedin)
+console.log(boolean)
+//""=>false
+//"asdhbba"=>true 
+let number=13
+let string=String(number);
+//number toh bangya but still string hai
