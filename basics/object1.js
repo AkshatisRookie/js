2ec43f98e0b4d3a7b090a2325e28f5cac816402f
Akshat Seth <u23ee101@eed.svnit.ac.in>
2025-01-11 19:49:09 +0530

    objects

--- basics/object1.js
+++ basics/object1.js
@@ -1,19 +1,21 @@
+//not singleton type
+
 const my=Symbol("key1") //syntax imp ques is declare sybol use it in ibject then call it?/
 const user={       //object 
     name:"akshat", 
-    [my]:123,
+    [my]:123, //tto acess keep it in []
     age:30,
     email:"akjsdasdk@"
 }
 
 console.log(user.name);
-console.log(user["name"]);
-console.log(user[my]);
+console.log(user["name"]); 
+console.log(user[my]); //for my symbol type
 console.log(typeof my)// my is symbol when used under user it is object
 user.name='aksh'//change the data
 //Object.freeze(user) //freeeze the data
 //++++++++++++++++funtions imp+++++++++++++++++++++++++
 user.greeting=function(){
-    console.log(`function operation, ${this.name},by akshat`);//imp syntax for writing value from another line to here
+    console.log(`function operation, ${this.name},by akshat`);//imp 'this' syntax for writing value from another line to here
 }
 console.log(user.greeting);
---
+++ basics/object2.js
@@ -0,0 +1,14 @@
+//const tinder=new Object() non singelton object
+const tinder={}
+tinder.id='abcd'
+tinder.name='aksh'
+tinder.isLoggedin=false
+
+console.log(Object.keys(tinder)) //list for data manipulation
+console.log(Object.values(tinder))
+//destructuring of objects
+const {name:fullname}=tinder
+console.log(fullname); //name changed 
+const func=({name})=>{ //arrow loops
+
+}
